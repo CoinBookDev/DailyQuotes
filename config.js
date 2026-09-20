783e33625a878873caa54ed2a43048c3bd3f8926
DailyQuotes <teungeselschap@gmail.com>
2026-09-20 13:18:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://gqebydtmqqrbryrblewc.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://gqebydtmqqrbryrblewc.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_JUgeAdSO--n3ahN3Qdr9qg_3YS0H779";
